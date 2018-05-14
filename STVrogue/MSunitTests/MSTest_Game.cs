4be6c5dcd268c8e6344e0a1052e3942d4d5c6fc7
Pepn <pepijnspampepijnspam@gmail.com>
2018-05-14 16:52:16 +0200

    game
    tests

---
+++ STVrogue/MSunitTests/MSTest_Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using STVRogue.GameLogic;
+using STVRogue.Utils;
+
+namespace UnitTests_STVRogue
+{
+    [TestClass]
+    public class MSTest_Game
+    {
+        [TestMethod]
+        public void MSTest_create_random_game()
+        {
+            uint difficultyLevel = (uint)RandomGenerator.rnd.Next(100);
+            uint nodeCapacityMultiplier = (uint)RandomGenerator.rnd.Next(5);
+            uint numberOfMonsters = (uint)RandomGenerator.rnd.Next(100);
+
+            for(int i = 0; i < 100; ++i)
+            {
+                Game game = new Game(difficultyLevel, nodeCapacityMultiplier, numberOfMonsters);
+            
+            }
+        }
+
+    }
+}
